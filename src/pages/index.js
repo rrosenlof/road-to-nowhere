e9b6d0ef99e7d552b45f9c4ecbfda50c6e270262
ross <rosenlof.ross@gmail.com>
2020-07-11 00:11:14 -0600

    loading images works

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,26 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import JSONData from "../../content/taco-bells.json"
+
+function getAddress(){
+  const len = Object.keys(JSONData).length;
+  console.log(len);
+  const rand = Math.floor(Math.random() * Math.floor(len));
+  console.log(rand)
+  console.log(JSONData[rand])
+  return [JSON.stringify(JSONData[rand]['latitude']), JSON.stringify(JSONData[rand]['longitude'])];
+}
+
+var obj = getAddress();
 
 const IndexPage = () => (
   <Layout>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <img src={`${process.env.API_URL}key=${process.env.KEY}&center=New+York&size=1100,500@2x`} alt='Selected map area'></img>
+    
+    <img style={{ width: `3000px` }}src={`${process.env.API_URL}key=${process.env.KEY}&center=${obj[0]},${obj[1]}&scalebar=true|bottom&imagetype=png&zoom=18&scalebar=false&traffic=false&size=1600,1600&type=sat`} alt='Selected map area'></img>
+    
+    <div>
+      {obj[0]}
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
