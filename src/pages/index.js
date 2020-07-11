1fd4c28c20f83fe9fdc3ace4df5908ce3c9890ef
ross <rosenlof.ross@gmail.com>
2020-07-10 22:52:31 -0600

    Clean up of project, adding data, test img

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,11 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      <img src={`${process.env.API_URL}key=${process.env.KEY}&center=New+York&size=1100,500@2x`} alt='Selected map area'></img>
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
