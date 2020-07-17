82522637d164c95697b35c8bbccb5923cac9d4eb
Ross Rosenlof <rrosenlo@byu.edu>
2020-07-17 14:10:57 -0600

    Tab index changes

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ const Header = ({ siteTitle }) => (
   <header
     style={{
       marginBottom: `1.45rem`,
-    }}
+    }} 
   >
     <div className="header"
       style={{
@@ -15,15 +15,15 @@ const Header = ({ siteTitle }) => (
       }}
     >
       <div className="header-left">
-        <Link style={{ textDecoration: `none`}} to="/">
+        <Link style={{ textDecoration: `none`}} to="/" tabIndex={-1}>
           <h2 style={{ fontSize: `2rem` }}>Road to Nowhere</h2>
         </Link>
       </div>
       <div className="header-right">
-        <Link style={{ textDecoration: `none` }} to="/settings/">
+        <Link style={{ textDecoration: `none` }} to="/settings/" tabIndex={-1}>
           <h2>Settings</h2>
         </Link>
-        <Link style={{ textDecoration: `none` }} to="/about/">
+        <Link style={{ textDecoration: `none` }} to="/about/" tabIndex={-1}>
           <h2>About</h2>
         </Link>
       </div>
